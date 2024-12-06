3a4e1879d3b41434b74f39d21f62907faf807dc6
sooraj suresh <soorajsureshcvr@gmail.com>
2024-12-06 15:33:17 +0530

    2620. Counter

---
+++ 2620-Counter.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    
+    return function() {
+        return n++;
+    };
+};
+
+
+const counter = createCounter(10)
+console.log(counter()) // 10
+console.log(counter()) // 11
+console.log(counter()) // 12
+
+
+const counter2 = createCounter(-2);
+console.log(counter()) // 10
+console.log(counter())// 11
+console.log(counter()) // 12
+        
